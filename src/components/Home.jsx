f91dd0431fc50153827c67b4d360b3f73323d403
Pranav Sinha <pran.sinha1.0@gmail.com>
2021-06-06 21:20:02 +0530

    Made Navbar and Home responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,29 @@
 import "../styles/Home.scss";
 
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 const Home = () => {
   return (
     <section id="Home">
-      <h1>Home</h1>
+      <div className="content">
+        <p className="greeting">HELLO,</p>
+        <p className="name">I’M Pranav Sinha</p>
+        <p className="intro">A Full-Stack Developer</p>
+        <p className="about">
+          I am a student based in India pursuing Bachelors in Technology in
+          Computer Science, interested in building things for the internet
+        </p>
+        <hr />
+        <a
+          href="https://www.linkedin.com/in/pranav-sinha/"
+          target="_blank"
+          rel="noreferrer">
+          <FaLinkedin className="logo" />
+        </a>
+        <a href="https://github.com/pran01" target="_blank" rel="noreferrer">
+          <FaGithub className="logo" />
+        </a>
+      </div>
     </section>
   );
 };
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,24 +2,39 @@ import "../styles/navbar.scss";
 import logo from "../assets/images/PS-Logo-Black.png";
 
 const Navbar = () => {
+  const toggleNavbar = () => {
+    const navBar = document.querySelector(".navbar-links");
+    const toggleBtn = document.querySelector(".toggle-navbar");
+    navBar.classList.toggle("active");
+    toggleBtn.classList.toggle("active");
+  };
   return (
     <nav id="navbar" className="navbar">
-      <div className="logo-container">
+      <a className="logo-container" href="#Home">
         <img src={logo} alt="Logo" className="logo" />
+      </a>
+      <div className="toggle-navbar" onClick={toggleNavbar}>
+        <span className="bar"></span>
       </div>
       <div className="navbar-links">
         <ul>
           <li>
-            <a href="#Skills">Skills</a>
+            <a href="#Skills" onClick={toggleNavbar}>
+              Skills
+            </a>
           </li>
           <li>
-            <a href="#Projects">Projects</a>
+            <a href="#Projects" onClick={toggleNavbar}>
+              Projects
+            </a>
           </li>
           <li>
-            <a href="#Contact">Contact</a>
+            <a href="#Contact" onClick={toggleNavbar}>
+              Contact
+            </a>
           </li>
           <li>
-            <a href="#" type="button" className="resume-btn">
+            <a href="#Home" type="button" className="resume-btn">
               Resume
             </a>
           </li>
