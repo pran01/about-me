df830b4d1b503b7a2026a9b8e72dcabf5eba7a2a
Pranav Sinha <pran.sinha1.0@gmail.com>
2021-06-12 01:19:52 +0530

    Added Languages and Libraries in Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,9 +1,38 @@
 import "../styles/Skills.scss";
 
+import Cpp from "../assets/icons/C++.png";
+import CSS from "../assets/icons/CSS.png";
+import Flask from "../assets/icons/Flask.png";
+import Git from "../assets/icons/Git.png";
+import HTML from "../assets/icons/HTML.png";
+import JS from "../assets/icons/JS.png";
+import Python from "../assets/icons/Python.png";
+import ReactLogo from "../assets/icons/React.png";
+import ReactNative from "../assets/icons/ReactNative.png";
+import Sass from "../assets/icons/sass.png";
+import SQLite from "../assets/icons/SQLite.png";
+
 const Skills = () => {
+  const languages = [HTML, CSS, JS, Python, Cpp];
+  const libraries = [Flask, ReactLogo, ReactNative, SQLite, Sass, Git];
   return (
     <section id="Skills">
-      <h1>Skills</h1>
+      <div className="content">
+        <p>What I can work with</p>
+        <hr align="left" size="0" />
+        <div className="languages">
+          <p>Languages</p>
+          {languages.map((item) => (
+            <img src={item} alt={`${item}`} className="skill-logo" />
+          ))}
+        </div>
+        <div className="libraries">
+          <p>Libraries/Frameworks/Tools</p>
+          {libraries.map((item) => (
+            <img src={item} alt={`${item}`} className="skill-logo" />
+          ))}
+        </div>
+      </div>
     </section>
   );
 };
