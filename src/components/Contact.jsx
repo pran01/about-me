801ff980bf55273b26ea2e813863695823cb4498
Pranav Sinha <pran.sinha1.0@gmail.com>
2021-06-19 21:54:23 +0530

    contact form inputs complete

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,35 @@
 import "../styles/Contact.scss";
 
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+
 const Contact = () => {
   return (
     <section id="Contact">
-      <h1>Contact</h1>
+      <div class="contact-container">
+        <div class="contact-form">
+          <input type="text" class="name" placeholder="Name" />
+          <input type="email" class="email" placeholder="Email" />
+          <textarea
+            type="textarea"
+            class="message"
+            placeholder="Your Message"
+          />
+          <a href="" class="send" type="button">
+            Send
+          </a>
+        </div>
+        <div class="profile-links">
+          <a
+            href="https://www.linkedin.com/in/pranav-sinha/"
+            target="_blank"
+            rel="noreferrer">
+            <FaLinkedin className="logo" />
+          </a>
+          <a href="https://github.com/pran01" target="_blank" rel="noreferrer">
+            <FaGithub className="logo" />
+          </a>
+        </div>
+      </div>
     </section>
   );
 };
