88877c96c08edbbb9bfa770c8a208a0eceb31967
Pranav Sinha <pran.sinha1.0@gmail.com>
2021-06-26 11:39:58 +0530

    Project Cards added

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,32 @@
+import "../styles/ProjectCard.scss";
+
+import { FaGithub, FaChrome } from "react-icons/fa";
+
+const ProjectCard = ({ name, image, about, github, live }) => {
+  return (
+    <div className="project-card-container">
+      <div className="project-card-main">
+        <img src={image} alt={name} className="project-card-image" />
+        <div className="project-card-overlay">
+          <p>{about}</p>
+
+          <a href="#Home">
+            <div className="project-card-link">
+              <FaGithub className="project-card-link-logo" />
+              <span className="project-card-link-name">Github</span>
+            </div>
+          </a>
+          <a href="#Home">
+            <div className="project-card-link">
+              <FaChrome className="project-card-link-logo" />
+              <span className="project-card-link-name">Live</span>
+            </div>
+          </a>
+        </div>
+      </div>
+      <div className="project-card-name">{name}</div>
+    </div>
+  );
+};
+
+export default ProjectCard;
