d076cb55260214535940af389a4c58bff84504e4
Pranav Sinha <pran.sinha1.0@gmail.com>
2021-06-26 16:48:07 +0530

    send button animation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,23 +1,60 @@
 import "../styles/Contact.scss";
 
-import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { FaLinkedin, FaGithub, FaTelegramPlane } from "react-icons/fa";
 
 import emailjs from "emailjs-com";
 import apiKey from "../config/emailkey";
+import { useState } from "react";
 
 const Contact = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
+
+  const changeName = (e) => {
+    setName(e.target.value);
+  };
+  const changeEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const changeMessage = (e) => {
+    setMessage(e.target.value);
+  };
+
   const sendEmail = (e) => {
     e.preventDefault(); // Prevents default refresh by the browser
-    emailjs
-      .sendForm(apiKey.SERVICE_ID, apiKey.TEMPLATE_ID, e.target, apiKey.USER_ID)
-      .then(
-        (result) => {
-          alert("Message Sent, We will get back to you shortly", result.text);
-        },
-        (error) => {
-          alert("An error occurred, Please try again", error.text);
-        }
-      );
+    const nameField = document.querySelectorAll(".sender-name")[0];
+    const emailField = document.querySelectorAll(".sender-email")[0];
+    const messageField = document.querySelectorAll(".sender-message")[0];
+    const sendBtn = document.querySelector(".send");
+
+    console.log(nameField.value);
+
+    if (nameField.value && emailField.value && messageField.value) {
+      sendBtn.classList.toggle("active");
+      emailjs
+        .sendForm(
+          apiKey.SERVICE_ID,
+          apiKey.TEMPLATE_ID,
+          e.target,
+          apiKey.USER_ID
+        )
+        .then(
+          (result) => {
+            sendBtn.classList.toggle("active");
+            alert("Message Sent, We will get back to you shortly", result.text);
+            setName("");
+            setEmail("");
+            setMessage("");
+          },
+          (error) => {
+            sendBtn.classList.toggle("active");
+            alert("An error occurred, Please try again", error.text);
+          }
+        );
+    } else {
+      alert("Please fill out all the details before sending the mail");
+    }
   };
   return (
     <section id="Contact">
@@ -26,24 +63,32 @@ const Contact = () => {
         <form className="contact-form" onSubmit={sendEmail}>
           <input
             type="text"
-            className="name"
+            className="sender-name"
             placeholder="Name"
             name="from_name"
+            value={name}
+            onChange={changeName}
           />
           <input
             type="email"
-            className="email"
+            className="sender-email"
             placeholder="Email"
             name="from_email"
+            value={email}
+            onChange={changeEmail}
           />
           <textarea
             type="textarea"
-            className="message"
+            className="sender-message"
             placeholder="Your Message"
             name="message"
+            value={message}
+            onChange={changeMessage}
           />
           <button className="send" type="submit">
-            Send
+            <FaTelegramPlane className="send-icon" />
+
+            <span>Send</span>
           </button>
           {/*
           TODO: 
